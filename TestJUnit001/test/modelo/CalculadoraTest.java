624b73f18961f65d3b5f6de8c9bb4677b734eb8b
unknown <nickelwxl@gmail.com>
2019-09-26 17:33:26 -0300

    version 1

---
+++ TestJUnit001/test/modelo/CalculadoraTest.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author MEGA
+ */
+public class CalculadoraTest {
+    
+    public CalculadoraTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of Sumar method, of class Calculadora.
+     */
+    @Test
+    public void testSumar() {
+        System.out.println("Sumar");
+        Calculadora instance = new Calculadora(8,2);
+        assertTrue(instance.Sumar()==10);
+    }
+
+    /**
+     * Test of Restar method, of class Calculadora.
+     */
+    @Test
+    public void testRestar() {
+        System.out.println("Restar");
+        Calculadora instance = new Calculadora(8,2);
+        assertTrue(instance.Restar()==6);
+    }
+
+    /**
+     * Test of Multiplicar method, of class Calculadora.
+     */
+    @Test
+    public void testMultiplicar() {
+        System.out.println("Multiplicar");
+        Calculadora instance = new Calculadora(8,2);
+        assertTrue(instance.Multiplicar()==16);
+    }
+
+    /**
+     * Test of Dividir method, of class Calculadora.
+     */
+    @Test
+    public void testDividir() {
+        System.out.println("Dividir");
+        Calculadora instance = new Calculadora(8,2);
+        assertTrue(instance.Dividir()==4);
+    }
+    
+}
